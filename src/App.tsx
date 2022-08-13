54190f7db2613e1339e10ee4a21c47d68c09971d
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-08-13 13:20:22 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div className="fixed w-full h-full bg-slate-900 grid place-items-center">
+      <main className="w-[54rem] h-[35rem] rounded-md bg-slate-600"></main>
+    </div>
+  );
+}
+
+export default App;
